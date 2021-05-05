424d582f12a3bcb17c7be0fc6784e53f3d157901
Rajandeep <>
2021-05-04 21:18:57 -0700

    Update to adhere to styleguides

--- src/js/components/ShowKeyBoardShortcuts.js
+++ src/js/components/ShowKeyBoardShortcuts.js
@@ -4,12 +4,12 @@ import Modal from 'react-modal';
 
 const customStyles = {
   content: {
-    width: "50%",
-    height: "65%",
-    padding: "20px",
-    margin: "auto",
-    textAlign: "left",
-  }
+    height: '65%',
+    margin: 'auto',
+    padding: '20px',
+    textAlign: 'left',
+    width: '50%',
+  },
 };
 
 const ShowKeyBoardShortcuts = ({ isOpen, close }) => {
@@ -19,64 +19,72 @@ const ShowKeyBoardShortcuts = ({ isOpen, close }) => {
       style={customStyles}
       contentLabel="Keyboard Shortcuts"
     >
-      <div style={{ display: "flex" }}>
-        <button style={{ marginLeft: "auto" }} onClick={close}>CLOSE</button>
+      <div style={{ display: 'flex' }}>
+        <button style={{ marginLeft: 'auto' }} onClick={close}>
+          CLOSE
+        </button>
       </div>
 
-      <h4><b>Keyboard Shortcuts</b></h4>
+      <h4>
+        <b>Keyboard Shortcuts</b>
+      </h4>
       <table>
         <tr>
           <th>L</th>
-          <th style={{ fontWeight: "normal" }}>Toggle Larivar</th>
+          <th style={{ fontWeight: 'normal' }}>Toggle Larivar</th>
         </tr>
         <tr>
-          <th><b>Shift + L</b></th>
-          <th style={{ fontWeight: "normal" }}>Toggle Larivar Assist</th>
+          <th>Shift + L</th>
+          <th style={{ fontWeight: 'normal' }}>Toggle Larivar Assist</th>
         </tr>
         <tr>
-          <th><b>V</b></th>
-          <th style={{ fontWeight: "normal" }}>Toggle Vishraams</th>
+          <th>V</th>
+          <th style={{ fontWeight: 'normal' }}>Toggle Vishraams</th>
         </tr>
         <tr>
-          <th><b>A</b></th>
-          <th style={{ fontWeight: "normal" }}>Toggle Autoscroll Mode</th>
+          <th>A</th>
+          <th style={{ fontWeight: 'normal' }}>Toggle Autoscroll Mode</th>
         </tr>
         <tr>
-          <th><b>Ctrl + Shift + C</b></th>
-          <th style={{ fontWeight: "normal" }}>Center Align</th>
+          <th>Ctrl + Shift + C</th>
+          <th style={{ fontWeight: 'normal' }}>Center Align</th>
         </tr>
         <tr>
-          <th><b>E</b></th>
-          <th style={{ fontWeight: "normal" }}>Toggle English Translation</th>
+          <th>E</th>
+          <th style={{ fontWeight: 'normal' }}>Toggle English Translation</th>
         </tr>
         <tr>
-          <th><b>T</b></th>
-          <th style={{ fontWeight: "normal" }}>Toggle Punjabi Translation</th>
+          <th>T</th>
+          <th style={{ fontWeight: 'normal' }}>Toggle Punjabi Translation</th>
         </tr>
         <tr>
-          <th><b>S</b></th>
-          <th style={{ fontWeight: "normal" }}>Toggle Spanish Translation</th>
+          <th>S</th>
+          <th style={{ fontWeight: 'normal' }}>Toggle Spanish Translation</th>
         </tr>
         <tr>
-          <th><b>Shift + E</b></th>
-          <th style={{ fontWeight: "normal" }}>Toggle English Transliteration</th>
+          <th>Shift + E</th>
+          <th style={{ fontWeight: 'normal' }}>
+            Toggle English Transliteration
+          </th>
         </tr>
         <tr>
-          <th><b>Shift + S</b></th>
-          <th style={{ fontWeight: "normal" }}>Toggle Shahmukhi Transliteration</th>
+          <th>Shift + S</th>
+          <th style={{ fontWeight: 'normal' }}>
+            Toggle Shahmukhi Transliteration
+          </th>
         </tr>
         <tr>
-          <th><b>Shift + H</b></th>
-          <th style={{ fontWeight: "normal" }}>Toggle Hindi Transliteration</th>
+          <th>Shift + H</th>
+          <th style={{ fontWeight: 'normal' }}>Toggle Hindi Transliteration</th>
         </tr>
       </table>
     </Modal>
-  )
-}
+  );
+};
 
 ShowKeyBoardShortcuts.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   close: PropTypes.func.isRequired,
-}
+};
 
 export default ShowKeyBoardShortcuts;
